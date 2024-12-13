09bc1955d637978e5cf97b8e0e30b78b84480201
silvino boy louis <programmerbeast296@gmail.com>
2024-12-12 23:43:27 -0800

    version 1

---
+++ javascript.js
@@ -0,0 +1,12 @@
+//function shutdown(){
+  //if(confirm("hello"))
+  //alert("hello world")
+//};
+function shutdown(){
+let i = 10;
+ if(close() /* you can put the method that you want */( i >= 10 || i <= 2 )){
+alert("your incorrect in q == 2, so please check your code very well next time");
+ }else{
+  alert(" you"  + "i" + " is correct " + "😒😊👌🤷‍♀️😘😂╰(*°▽°*)╯" );
+ }
+};
